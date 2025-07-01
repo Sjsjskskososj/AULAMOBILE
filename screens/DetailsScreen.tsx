fd244c8fecedc29064d757a984559727c07e33a4
carics <azevedoarthur2009@gmail.com>
2025-07-01 14:42:46 -0300

    Update DetailsScreen.tsx

--- screens/DetailsScreen.tsx
+++ screens/DetailsScreen.tsx
@@ -23,7 +23,7 @@ export default function DetailsScreen({ navigation, route }: Props) {
 
 //aprende a commitar arthur
 //pobre fodido
-
+//cala tua boca mindigo
 const styles = StyleSheet.create({
   container: {
     flex: 1,
